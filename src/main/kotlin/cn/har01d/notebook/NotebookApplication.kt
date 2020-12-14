a175e53564b6149b0654494dca8fbde5d841bcc1
Harold <power721@163.com>
2020-12-14 18:36:00 +0800

    创建默认分类

--- src/main/kotlin/cn/har01d/notebook/NotebookApplication.kt
+++ src/main/kotlin/cn/har01d/notebook/NotebookApplication.kt
@@ -1,10 +1,19 @@
 package cn.har01d.notebook
 
+import cn.har01d.notebook.entity.Category
+import cn.har01d.notebook.entity.CategoryRepository
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class NotebookApplication
+class NotebookApplication(private val categoryRepository: CategoryRepository) : CommandLineRunner {
+    override fun run(vararg args: String?) {
+        if (categoryRepository.count() == 0L) {
+            categoryRepository.save(Category("默认分类", ""))
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<NotebookApplication>(*args)
