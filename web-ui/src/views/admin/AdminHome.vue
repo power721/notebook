14ffce0a86459e7fb00a34b741bd646d1fe38a85
Harold <power721@163.com>
2022-02-04 15:36:24 +0800

    手机模式：菜单显示图标

--- web-ui/src/views/admin/AdminHome.vue
+++ web-ui/src/views/admin/AdminHome.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="ui fluid container">
 
-    <div class="ui pointing secondary menu">
+    <div class="ui pointing secondary menu" v-if="wide">
       <router-link class="item" to="/admin/info">系统信息</router-link>
       <router-link class="item" to="/admin/stats">统计信息</router-link>
       <router-link class="item" to="/admin/audit">操作日志</router-link>
       <router-link class="item" to="/admin/menu">菜单管理</router-link>
       <router-link class="item" to="/admin/config">站点设置</router-link>
+    </div>
+    <div class="ui pointing secondary menu" v-else>
+      <router-link class="item" to="/admin/info" title="系统信息"><i class="info icon"></i></router-link>
+      <router-link class="item" to="/admin/stats" title="统计信息"><i class="database icon"></i></router-link>
+      <router-link class="item" to="/admin/audit" title="操作日志"><i class="bell icon"></i></router-link>
+      <router-link class="item" to="/admin/menu" title="菜单管理"><i class="list icon"></i></router-link>
+      <router-link class="item" to="/admin/config" title="站点设置"><i class="setting icon"></i></router-link>
     </div>
 
     <transition name="fade" mode="out-in">
@@ -20,8 +27,11 @@
 
   @Component
   export default class AdminHome extends Vue {
+    wide = window.innerWidth > 720
     mounted() {
-      //
+      window.addEventListener('resize', () => {
+        this.wide = window.innerWidth > 720
+      })
     }
   }
 </script>
--- web-ui/src/views/user/UserHome.vue
+++ web-ui/src/views/user/UserHome.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="ui fluid container">
 
-    <div class="ui pointing secondary menu">
+    <div class="ui pointing secondary menu" v-if="wide">
       <router-link class="item" to="/user/notes">我的笔记</router-link>
       <router-link class="item" to="/user/notebooks">我的笔记本</router-link>
       <router-link class="item" to="/user/trash">回收站</router-link>
       <router-link class="item" to="/user/security">账号安全</router-link>
       <router-link class="item" to="/user/info">用户设置</router-link>
+    </div>
+    <div class="ui pointing secondary menu" v-else>
+      <router-link class="item" to="/user/notes" title="我的笔记">
+        <i class="file alternate outline icon"></i>
+      </router-link>
+      <router-link class="item" to="/user/notebooks" title="我的笔记本"><i class="book icon"></i></router-link>
+      <router-link class="item" to="/user/trash" title="回收站"><i class="trash icon"></i></router-link>
+      <router-link class="item" to="/user/security" title="账号安全"><i class="shield alternate icon"></i></router-link>
+      <router-link class="item" to="/user/info" title="用户设置"><i class="setting icon"></i></router-link>
     </div>
 
     <transition name="fade" mode="out-in">
@@ -20,6 +29,12 @@
 
   @Component
   export default class UserHome extends Vue {
+    wide = window.innerWidth > 720
+    mounted() {
+      window.addEventListener('resize', () => {
+        this.wide = window.innerWidth > 720
+      })
+    }
   }
 </script>
 
